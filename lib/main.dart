736cc48c36c5004630f2970c67e791164913e303
Caxton Mbuvi <caxtonmbuvi@Caxtons-MacBook-Pro.local>
2023-12-08 08:19:32 +0300

    Google Maps improvement commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       designSize: const Size(390, 844),
       builder: (context, child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Locator',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
